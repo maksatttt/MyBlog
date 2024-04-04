a44dcf9c3823c7066264ad401c329d2608c0967a
Maksat <akzholtoy_uulu_m@iuca.kg>
2024-04-05 03:00:19 +0600

    first commit

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,10 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -24,4 +28,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-    
